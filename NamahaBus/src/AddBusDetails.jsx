97346e82b2efcfd854ce6fd5392c38b0a38da7ae
hemanth <hemanthwebdevelopment@gmail.com>
2026-07-16 16:02:32 +0530

    remove commit in addbusdetails

--- NamahaBus/src/AddBusDetails.jsx
+++ NamahaBus/src/AddBusDetails.jsx
@@ -92,41 +92,8 @@ const days = [
     }
   };
   return (
-    // <div className="min-h-screen bg-slate-100 flex items-center justify-center py-10">
-    //   <div className="w-full max-w-6xl bg-white rounded-3xl shadow-2xl overflow-hidden">
-       /* Header */
-        // <div className="bg-gradient-to-r from-blue-600 to-cyan-500 p-8 text-center">
-        //   <div className="flex items-center gap-4">
-        //     <div className="bg-white/20 p-4 rounded-2xl">
-        //       <Bus size={40} className="text-white" />
-        //     </div>
-        //      <div>
-        //       <h1 className="text-4xl font-bold text-white">
-        //         Add Bus Services
-        //       </h1>
-        //          <p className="text-white/80">
-        //         Create a new bus service
-        //       </p>
-        //     </div>
-        //   </div>
-        // </div>
-        //  <form onSubmit={handleSubmit} className="p-4 space-y-8">
-          /* Route Information */
-          // <div>
-          //   <h2 className="text-xl font-semibold mb-4 mt-4">
-          //     Route Information
-          //   </h2>
-          // <div className="grid md:grid-cols-2 gap-5">
-          //     <InputField icon={<MapPin size={18} />} name="from" placeholder="From" value={formData.from} onChange={handleChange}/>
-
-          //     <InputField icon={<MapPin size={18} />} name="to" placeholder="To" value={formData.to} onChange={handleChange}  />
-
-          //     <InputField  icon={<Hash size={18} />} name="serviceNo" placeholder="Service Number" value={formData.serviceNo} onChange={handleChange} />
-
-          //     <InputField icon={<Bus size={18} />} name="plate" placeholder="Plate Number" value={formData.plate} onChange={handleChange} />
-          //   </div>
-          // </div>
-          <div className="addbus-page">
+   
+   <div className="addbus-page">
   <div className="addbus-container">
 
     {/* Header */}
@@ -392,21 +359,9 @@ const days = [
 
     )}
 
-    
-
 </div>
-          {/* Pricing */}
-          {/* <div>
-            <h2 className="text-xl font-semibold mb-4 mt-4">
-              Pricing & Seats
-            </h2>
-
-            <div className="grid md:grid-cols-2 gap-5">
-              <InputField icon={<IndianRupee size={18} />} type="number" name="price" placeholder="Price" value={formData.price}  onChange={handleChange}  />
-              <InputField icon={<Armchair size={18} />} type="number" name="seatsLeft" placeholder="Seats Left" value={formData.seatsLeft} onChange={handleChange} />
-            </div>
-          </div> */}
-          {/* Pricing & Seats */}
+   
+    {/* Pricing & Seats */}
 
 <div className="form-section">
 
@@ -437,90 +392,6 @@ const days = [
   </div>
 
 </div>
-
-          {/* Preview */}
-          {/* <div className="bg-slate-50 rounded-3xl border p-6">
-            <h3 className="text-xl font-bold mb-4 mt-4">
-              🎫 Bus Preview
-            </h3>
-
-            <div className="grid md:grid-cols-2 gap-4 p-4">
-
-              <PreviewRow label="Service No" value={formData.serviceNo} />
-              <PreviewRow label="Bus Name" value={formData.name} />
-              <PreviewRow label="Company" value={formData.companyName} />
-              <PreviewRow label="Type" value={formData.type} />
-              <PreviewRow label="From" value={formData.from} />
-              <PreviewRow label="To" value={formData.to} />
-              <PreviewRow label="Plate" value={formData.plate} />
-              <PreviewRow label="Duration" value={formData.duration} />
-              <PreviewRow label="Price" value={`₹${formData.price}`} />
-              <PreviewRow label="Seats Left" value={formData.seatsLeft} />
-            </div>
-          </div>
-
-          <button
-            type="submit"
-            className=" mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white py-4 rounded-2xl font-semibold text-lg"
-          onClick={()=>navgiate("/BusDetails")}> 
-            🚍 Add Bus Service
-          </button>
-        <button
-            type="submit"
-            className=" mt-4 w-full bg-green-600 hover:bg-green-700 text-white py-4 rounded-2xl font-semibold text-lg"
-          onClick={()=>navgiate("/BusDetails")}>
-            ➡️ next page
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-}
-function InputField({
-  icon,
-  type = "text",
-  name,
-  placeholder,
-  value,
-  onChange,
-}) {
-  const showIcon =
-    type !== "date" &&
-    type !== "datetime-local";
-
-  return (
-    <div className="relative">
-      {showIcon && (
-        <div className="absolute left-4 top-1/2 -translate-y-1/2 text-slate-400">
-          {icon}
-        </div>
-      )}
-
-      <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        required
-        className={`text-center w-full border border-slate-300 rounded-2xl py-3 pr-4 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-          showIcon ? "pl-12" : "px-4"
-        }`}
-      />
-    </div>
-  );
-} */}
-
-{/* function PreviewRow({ label, value }) {
-  return (
-    <div className="flex justify-between border-b py-2">
-      <span className="text-slate-500">{label}</span>
-      <span className="font-semibold">
-        {value || "-"}
-      </span>
-    </div>
-  );
-} */}
 
 {/* Preview */}
 
